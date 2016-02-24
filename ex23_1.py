b6a898dbf2e758b6a7e2b7a089271ed754a0ce56
jarturomora <jarturomora@gmail.com>
2016-02-24 07:24:35 -0600

    Solution finished for the fogotten words problem

--- ex23_1.py
+++ ex23_1.py
@@ -4,42 +4,66 @@
 
 # This function validates if a string complains with the constraint
 # 1 <= length of any string in the input <= 5
+import re
+
 def word_len(word):
-    valid_word = false
-    if (len(word) > 1 & len(word) <= 5):
-        valid_word = true
+    valid_word = False
+    if (len(word) > 1 and len(word) <= 5):
+        valid_word = True
     return valid_word
 
 in_file = open("input1.txt")
 
 num_tests = int(in_file.readline())
 # Valitate constain 1 <= num_tests <= 20
-if (num_tests >= 1 & num_tests <= 20):
-    for test in range(1, num_tests):
+if (num_tests >= 1 and num_tests <= 20):
+    for test in range(0, num_tests):
         # Read text case information
         test_case = str.split(in_file.readline())
         # Validate constrain 1 <= num_strings <= 100
         num_strings = int(test_case[0])
-        if (num_strings >= 1 & num_strings <= 100):
+        if (num_strings >= 1 and num_strings <= 100):
             # Validate constrain 1 <= num_phrases <= 50
             num_phrases = int(test_case[1])
-            if (num_phrases >= 1 & num_phrases <= 50):
+            if (num_phrases >= 1 and num_phrases <= 50):
                 # Read the words in the forgotten language
                 forgotten_words = str.split(in_file.readline())
+                # Create a list to define if a forgotten word has been found
+                # in modern language
+                forgotten_words_found = []
+                for i in range(0, len(forgotten_words)):
+                    forgotten_words_found.append("No")
                 # Start reading all the phrases in the modern language
-                for phrase in range (1, num_phrases):
+                for phrase in range (0, num_phrases):
                     current_phrase = str.split(in_file.readline())
                     # Validate 1 <= phrase_len <= 50
                     phrase_len = int(current_phrase[0])
-                    if (phrase_len >= 1 & phrase_len <=50):
-                        # Validating constrain 1 <= word_len <= 5
-                        print "We can validate the case :", test
-                    else:
-                        print "Phrase %d can't be analyzed: %r" % (phrase,
-                        current_phrase)                        
-            else:
-                print "Invalid number of phrases in the modern language."
-        else:
-            print "Invalid number of strings in the forgotten language."
-else:
-    print "Invalid number of test cases."
+                    if (phrase_len >= 1 and phrase_len <=50):
+                        # Look for the existense of every forgotten word in
+                        # the current phrase
+                        for i in range(0, len(forgotten_words)):
+                            if (word_len(forgotten_words[i])):
+                                for j in range(0, phrase_len):
+                                    if (word_len(current_phrase[j])):
+                                        # Was the word found before?
+                                        if (forgotten_words_found[i] == "No"):
+                                            for k in range(1, phrase_len):
+                                                if (word_len(current_phrase[k])):
+                                                    if (forgotten_words[i] 
+                                                        == current_phrase[k]):
+                                                        forgotten_words_found[i] = "Yes"
+                                    #else:
+                                    #    print "PW: %r is invalid" % current_phrase[j]
+                            #else:
+                            #    print "FG: %r is invalid." % forgotten_words[i]                        
+                    #else:
+                    #    print "Phrase %d can't be analyzed:%r" % (phrase,
+                    #    current_phrase)                        
+                #print "Case %d results: %r" % (test, str(forgotten_words_found))
+                print ' '.join(forgotten_words_found)
+            #else:
+            #    print "Invalid number of phrases in the modern language."
+        #else:
+        #    print "Invalid number of strings in the forgotten language."
+#else:
+#    print "Invalid number of test cases."
