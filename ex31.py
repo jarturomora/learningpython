57bc4ccb176637fc200810fdcdadd86ea30becbc
jarturomora <jarturomora@gmail.com>
2016-02-26 07:22:21 -0600

    ex31 done

---
+++ ex31.py
@@ -0,0 +1,53 @@
+print """
+You enter a dark room with two doors.
+Do you go through door #1, door #2, or door #3?
+"""
+
+door = raw_input("> ")
+
+if door == "1":
+    print "There's a giant bear here eating a cheese cake. What do you do?"
+    print "1. Take the cake."
+    print "2. Scream at the bear."
+    print "3. Sing a song."
+
+    bear = raw_input("> ")
+
+    if bear == "1":
+        print "The bear eats your face off. Good job!"
+    elif bear == "2":
+        print "The bear easts your legs off. Good job!"
+    else:
+        print "Well, doing %s is probably better. Bear runs away." % bear
+
+elif door == "2":
+    print "You stare into the endless abyss at Cthulhu's retina."
+    print "1. Blueberries."
+    print "2. Yellow jacket clothespins."
+    print "3. Understanding revolvers yelling melodies."
+
+    insanity = raw_input("> ")
+
+    if insanity == "1" or insanity == "2":
+        print "Your body survives powered by a mind of jello. Good job!"
+    else:
+        print "The insanity rots your eyes into a pool of muck. Good job!"
+
+elif door == "3":
+    print "You find a distracted fury bear. What music style you choose?"
+    print "1. Reggae."
+    print "2. Metal."
+    print "3. Classical."
+
+    music = raw_input("> ")
+
+    if music == "1":
+        print "Great, the bear is fan of Bob Marley, you are safe now!"
+    elif music == "2":
+        print "Oh oh!, the brear hates metal, you are doomed!"
+    elif music == "3":
+        print "Ups! classical music doesn't have lirics and the bear is not happy!"
+    else:
+        print "Since you failed to sing a nice song, the bear eats you!"
+else:
+    print "You stumble around and fall on a knife and die. Good job!"
