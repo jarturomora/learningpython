aee765946af004eb855b9c9e9427c8443f909cfd
jarturomora <jarturomora@gmail.com>
2016-03-01 19:41:03 -0600

    ex47 done, it's better to pick the code from the book website

--- ex47/tests/ex47_tests.py
+++ ex47/tests/ex47_tests.py
@@ -7,14 +7,14 @@ def test_room():
                 """This room has gold in it you can grab. There's a
                 doo to the north.""")
     assert_equal(gold.name, "GoldRoom")
-    assert_equal(gold.paths, [])
+    assert_equal(gold.paths, {})
 
 def test_room_paths():
     center = Room("Center", "Test room in the center.")
     north = Room("North", "Test room in the north.")
     south = Room("South", "Test room in the south.")
 
-    center.add_paths({'nort': north, 'south': south})    
+    center.add_paths({'north': north, 'south': south})    
     assert_equal(center.go('north'), north)
     assert_equal(center.go('south'), south)
 
